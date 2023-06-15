2989322a51a151b89c1e3f83bad13ea99ec838eb
Snehasrg <sghongade642@gmail.com>
2023-06-15 22:09:11 +0530

    [sneha]Added Program for Arraylist

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Demo;
+using NlogConfigDemo;
+using System.Collections.Generic;
+
+namespace NlogConfigDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to NLog Configuration,Arraylist and Dictionary CRUD operation!");
+            Console.WriteLine("Select any one Option for Opreration");
+            Console.WriteLine("\n 1. For Nlog Config Example. \n 2. CRUD operation on ArrayList.");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+
+            {
+                case 2:
+                    ArrayListCRUD.ArraylistOperation();
+                    break;
+               
+
+            }
+        }
+    }
+}
